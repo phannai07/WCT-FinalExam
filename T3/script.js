591c42ccf5dd10b4daf29b81616135e2795e7613
phannai07 <phannaiseak07@gmail.com>
2025-01-18 14:46:22 +0700

    Add files via upload

---
+++ T3/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+try {
+  // Replace 'Siek Sophannai' with your full name
+  console.log('Student Name: Siek Sophannai');
+  // Declare x before assigning a value to it
+  let x = 10; 
+} catch (error) {
+  console.error(error.message); 
+}
+
+function demo() {
+  try {
+    // Declare y before assigning a value to it
+    let y = 20; 
+  } catch (error) {
+    console.error(error.message); 
+  }
+}
